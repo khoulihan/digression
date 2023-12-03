3c19a6a42e796a35f021f5bbc3e51e5507268945
Kevin Houlihan <kevin@crimsoncookie.com>
2023-12-03 15:51:34 +0100

    Implemented operator management.

--- addons/hyh.cutscene_graph/editor/controls/expressions/Operator.gd
+++ addons/hyh.cutscene_graph/editor/controls/expressions/Operator.gd
@@ -2,11 +2,96 @@
 extends MarginContainer
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+const VariableType = preload("../../../resources/graph/VariableSetNode.gd").VariableType
+
+const AddIcon = preload("res://addons/hyh.cutscene_graph/icons/icon_add.svg")
+const SubtractIcon = preload("res://addons/hyh.cutscene_graph/icons/icon_subtract.svg")
+const MultiplyIcon = preload("res://addons/hyh.cutscene_graph/icons/icon_close.svg")
+const DivideIcon = preload("res://addons/hyh.cutscene_graph/icons/icon_divide.svg")
+
+@onready var _options : OptionButton = get_node("OperatorOptionButton")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+@export
+var variable_type : VariableType
+
+@export
+var operator_type : OperatorType
+
+
+enum ExpressionOperators {
+	BOOL_AND,
+	BOOL_OR,
+	NUMERIC_ADD,
+	NUMERIC_SUBTRACT,
+	NUMERIC_MULTIPLY,
+	NUMERIC_DIVIDE,
+	STRING_CONCATENATE,
+	STRING_CONCATENATE_WITH_SPACE,
+}
+
+enum OperatorType {
+	OPERATION,
+	COMPARISON,
+}
+
+
+func configure():
+	_options.clear()
+	if operator_type == OperatorType.COMPARISON:
+		_populate_comparison_operators()
+		return
+	
+	match variable_type:
+		VariableType.TYPE_BOOL:
+			_populate_boolean_operators()
+		VariableType.TYPE_INT, VariableType.TYPE_FLOAT:
+			_populate_numeric_operators()
+		VariableType.TYPE_STRING:
+			_populate_string_operators()
+
+
+func _populate_boolean_operators():
+	_options.add_item("And", ExpressionOperators.BOOL_AND)
+	_options.add_item("Or", ExpressionOperators.BOOL_OR)
+
+
+func _populate_numeric_operators():
+	_options.add_icon_item(
+		AddIcon,
+		"",
+		ExpressionOperators.NUMERIC_ADD
+	)
+	_options.add_icon_item(
+		SubtractIcon,
+		"",
+		ExpressionOperators.NUMERIC_SUBTRACT
+	)
+	_options.add_icon_item(
+		MultiplyIcon,
+		"",
+		ExpressionOperators.NUMERIC_MULTIPLY
+	)
+	_options.add_icon_item(
+		DivideIcon,
+		"",
+		ExpressionOperators.NUMERIC_DIVIDE
+	)
+
+
+func _populate_string_operators():
+	_options.add_icon_item(
+		AddIcon,
+		"",
+		ExpressionOperators.STRING_CONCATENATE
+	)
+	_options.add_icon_item(
+		AddIcon,
+		"(with space)",
+		ExpressionOperators.STRING_CONCATENATE_WITH_SPACE
+	)
+
+
+func _populate_comparison_operators():
+	# TODO: Add the comparison operators
 	pass
--- addons/hyh.cutscene_graph/editor/controls/expressions/OperatorExpression.gd
+++ addons/hyh.cutscene_graph/editor/controls/expressions/OperatorExpression.gd
@@ -1,3 +1,67 @@
 @tool
 extends "res://addons/hyh.cutscene_graph/editor/controls/expressions/GroupExpression.gd"
 
+
+const Operator = preload("res://addons/hyh.cutscene_graph/editor/controls/expressions/Operator.tscn")
+const OperatorClass = preload("res://addons/hyh.cutscene_graph/editor/controls/expressions/Operator.gd")
+
+
+func _add_to_bottom(child):
+	super(child)
+	_rectify_operators()
+
+
+func _remove_child_requested(child):
+	super(child)
+	_rectify_operators()
+
+
+func _rectify_operators():
+	# Go through all children and ensure that every pair of expressions has
+	# exactly one operator between them.
+	var children = get_children().slice(0, -1)
+	var to_remove = []
+	var previous = null
+	for child in children:
+		if child is OperatorClass:
+			if previous == null:
+				to_remove.append(child)
+				continue
+			if previous is OperatorClass:
+				# Decide which operator has precedence and remove the other
+				# Set previous to the winner
+				var winner = _operator_to_keep(previous, child, to_remove)
+				previous = winner
+			else:
+				previous = child
+			continue
+		
+		# child is Expression
+		if previous == null:
+			previous = child
+			continue
+		if not previous is OperatorClass:
+			# Insert an operator between previous and child
+			var operator = Operator.instantiate()
+			operator.operator_type = OperatorClass.OperatorType.OPERATION
+			operator.variable_type = type
+			add_child(operator)
+			move_child(operator, child.get_index())
+			operator.configure()
+		previous = child
+	
+	# Make sure an operator is not left dangling
+	if previous != null:
+		if previous is OperatorClass:
+			to_remove.append(previous)
+	
+	for child in to_remove:
+		remove_child(child)
+
+
+func _operator_to_keep(previous, current, to_remove):
+	if previous.operator_type > current.operator_type:
+		to_remove.append(current)
+		return previous
+	to_remove.append(previous)
+	return current
