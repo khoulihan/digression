a875ce002f983fef0a20f8081f6df98da8411490
Kevin Houlihan <kevin@crimsoncookie.com>
2024-02-23 13:30:50 +0100

    Added new root node indicator.

--- addons/hyh.cutscene_graph/editor/nodes/EditorGraphNodeBase.gd
+++ addons/hyh.cutscene_graph/editor/nodes/EditorGraphNodeBase.gd
@@ -17,19 +17,30 @@ signal modified()
 @export var is_root : bool : set = set_root
 @export var node_resource: GraphNodeBase
 
+var RootIndicator: TextureRect
+
 var graph
 
 
 func _ready():
-	var titlebar = get_titlebar_hbox()
-	var close_button = Button.new()
+	var titlebar := get_titlebar_hbox()
+	var close_button := Button.new()
 	close_button.text = ""
 	close_button.icon = CLOSE_ICON
 	close_button.flat = true
 	close_button.grow_horizontal = Control.GROW_DIRECTION_BOTH
 	close_button.size_flags_horizontal = Control.SIZE_SHRINK_END
 	close_button.pressed.connect(_close_button_pressed)
+	var root_indicator := TextureRect.new()
+	root_indicator.texture = get_theme_icon("PinJoint3D", "EditorIcons")
+	root_indicator.visible = false
+	root_indicator.modulate = Color("#2e2e2e")
+	root_indicator.stretch_mode = TextureRect.STRETCH_KEEP_CENTERED
+	root_indicator.expand_mode = TextureRect.EXPAND_KEEP_SIZE
+	RootIndicator = root_indicator
 	titlebar.add_child(close_button)
+	titlebar.add_child(root_indicator)
+	titlebar.move_child(root_indicator, 0)
 
 
 func configure_for_node(g, n):
@@ -62,6 +73,7 @@ func set_root(value: bool):
 	#else:
 		#self.overlay = GraphNode.OVERLAY_DISABLED
 	is_root = value
+	RootIndicator.visible = is_root
 
 
 func _on_gui_input(ev):
