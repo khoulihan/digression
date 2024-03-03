03f1f04a94f88c8a74d414c1384ecda6e59f0059
Kevin Houlihan <kevin@crimsoncookie.com>
2024-03-03 14:08:25 +0100

    Added doc comments to CutsceneVariableStore class.

--- addons/hyh.cutscene_graph/editor/CutsceneVariableStore.gd
+++ addons/hyh.cutscene_graph/editor/CutsceneVariableStore.gd
@@ -1,22 +1,26 @@
 @tool
 extends Node
-
+## Stores variables for use in dialogue graphs.
 
 # Exported so that a set of default values can be set.
 @export var store_data: Dictionary
 
 
+## Get a variable from the store.
 func get_variable(variable_name):
 	return store_data.get(variable_name)
 
 
+## Set the value of a variable in the store.
 func set_variable(variable_name, value):
 	store_data[variable_name] = value
 
 
+## Check if the store contains the specified variable.
 func has_variable(variable_name):
 	return store_data.has(variable_name)
 
 
+## Clear the store completely.
 func clear():
 	store_data.clear()
