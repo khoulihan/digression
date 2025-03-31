73c21b2db462be2dd78e1717f138a0373a1c1290
Kevin Houlihan <kevin@crimsoncookie.com>
2025-03-31 10:43:38 +0200

    Switched to a placeholder instead of default text for comments.

--- addons/hyh.digression/resources/graph/CommentNode.gd
+++ addons/hyh.digression/resources/graph/CommentNode.gd
@@ -3,7 +3,7 @@ extends "GraphNodeBase.gd"
 ## A node for a comment. It cannot be connected to other nodes.
 
 ## The comment text.
-@export var comment: String = "Enter your comment here..."
+@export var comment: String = ""
 
 # This is used only for recreating the node state in the editor
 @export var size: Vector2
